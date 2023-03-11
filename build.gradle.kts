e9917602913ef0542cf28c2f25a113944f2f02b0
Tofaa <82680183+Tofaa2@users.noreply.github.com>
2023-03-12 01:38:31 +0400

    idk whats wrong with jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 }
 
 dependencies {
-    api("com.github.Minestom:Minestom:a9e319f961")
+    api("com.github.Minestom:Minestom:-SNAPSHOT")
     api("net.kyori:adventure-text-minimessage:4.12.0")
     api("com.github.Polar-Network:MiniMessenger:0265bf9331")
     api("com.google.guava:guava:30.1.1-jre")
