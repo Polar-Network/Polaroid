e82e9671eda1d0bb14f19768af96866cd140581c
Tofaa <82680183+Tofaa2@users.noreply.github.com>
2023-03-12 13:19:33 +0400

    unbump 1.19.3 cause idk whats wrong with it

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 }
 
 dependencies {
-    api("com.github.hollow-cube:Minestom:e6d4a2cc91")
+    api("com.github.Minestom.Minestom:Minestom:a9e319f961")
     api("net.kyori:adventure-text-minimessage:4.12.0")
     api("com.github.Polar-Network:MiniMessenger:0265bf9331")
     api("com.google.guava:guava:30.1.1-jre")
